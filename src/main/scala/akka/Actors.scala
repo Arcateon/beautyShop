0c78aac11208f9b413d0232056ddfe6bca8e2977
arcateon <arcateon@gmail.com>
2022-10-20 10:25:37 +0300

    fix actors sending

--- src/main/scala/akka/Actors.scala
+++ src/main/scala/akka/Actors.scala
@@ -1,17 +1,19 @@
 package akka
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import com.typesafe.scalalogging.Logger
 import mongo.MongoUtils
 import org.json4s.{Formats, JValue}
 import org.slf4j.LoggerFactory
 import utils.Cases.InputData
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Actors {
 
   implicit val formats: Formats = org.json4s.DefaultFormats
     .withLong.withDouble.withStrictOptionParsing
   private val logger = Logger(LoggerFactory.getLogger(this.getClass))
+  val system: ActorSystem = ActorSystem("System")
 
   class NailsActor extends Actor {
 
@@ -19,11 +21,18 @@ object Actors {
       case body: JValue =>
         val document = body.extract[InputData]
         val collection = MongoUtils.nailSessions
+        val senderActor = sender()
         MongoUtils.insertNewSession(collection, document.name, document.service,
-          document.date, document.time, document.phone)
-      case _ => logger.info("received unknown message")
+          document.date, document.time, document.phone) map {result =>
+          senderActor ! result
+        }
+
+      case _ =>
+        logger.info("received unknown message")
+        sender ! false
     }
   }
+  val nailsActor: ActorRef = system.actorOf(Props[NailsActor], name = "NailsActor")
 
   class HairActor extends Actor {
 
@@ -31,11 +40,17 @@ object Actors {
       case body: JValue =>
         val document = body.extract[InputData]
         val collection = MongoUtils.hairSessions
+        val senderActor = sender()
         MongoUtils.insertNewSession(collection, document.name, document.service,
-          document.date, document.time, document.phone)
-      case _ => logger.info("received unknown message")
+          document.date, document.time, document.phone) map { result =>
+          senderActor ! result
+        }
+      case _ =>
+        logger.info("received unknown message")
+        sender ! false
     }
   }
+  val hairActor: ActorRef = system.actorOf(Props[HairActor], name = "HairActor")
 
   class BrowsActor extends Actor {
 
@@ -43,10 +58,16 @@ object Actors {
       case body: JValue =>
         val document = body.extract[InputData]
         val collection = MongoUtils.browsSessions
+        val senderActor = sender()
         MongoUtils.insertNewSession(collection, document.name, document.service,
-          document.date, document.time, document.phone)
-      case _ => logger.info("received unknown message")
+          document.date, document.time, document.phone) map { result =>
+          senderActor ! result
+        }
+      case _ =>
+        logger.info("received unknown message")
+        sender ! false
     }
   }
+  val browsActor: ActorRef = system.actorOf(Props[BrowsActor], name = "BrowsActor")
 
 }
--- src/main/scala/akka/Routes.scala
+++ src/main/scala/akka/Routes.scala
@@ -1,17 +1,19 @@
 package akka
 
 import akka.Actors._
-import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.pattern.ask
+import akka.util.Timeout
 import org.json4s.Formats
 import org.json4s.jackson._
 
 import scala.concurrent.Future
 import com.typesafe.scalalogging._
-import mongo.MongoUtils
 import org.slf4j.LoggerFactory
 import utils.Cases.InputData
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
@@ -19,6 +21,7 @@ object Routes {
 
   implicit val formats: Formats = org.json4s.DefaultFormats
     .withLong.withDouble.withStrictOptionParsing
+  implicit val timeout: Timeout = Timeout(5 seconds)
   private val logger = Logger(LoggerFactory.getLogger(this.getClass))
 
   val route: Route = {
@@ -29,28 +32,31 @@ object Routes {
         val service = requestBody.service
 
         val resultFuture = {
-          MongoUtils.insertNewSession(MongoUtils.allSessions, requestBody.name, requestBody.service,
-            requestBody.date, requestBody.time, requestBody.phone) flatMap {result =>
-            if(result) {
-              val system = ActorSystem("System")
-                service match {
-                  case "nails" =>
-                    val nailsActor = system.actorOf(Props[NailsActor], name = "NailsActor")
-                    nailsActor ! requestJson
-                    Future.successful("{success: true, message: new nails session}")
-                  case "hairCut" =>
-                    val hairActor = system.actorOf(Props[HairActor], name = "HairActor")
-                    hairActor ! requestJson
-                    Future.successful("{success: true, message: new hair cut session}")
-                  case "brows" =>
-                    val browsActor = system.actorOf(Props[BrowsActor], name = "BrowsActor")
-                    browsActor ! requestJson
-                    Future.successful("{success: true, message: new brows session}")
-                  case _ =>
-                    logger.info("unknown service")
-                    Future.successful("{success: false, message: unknown service}")
-                }
-            } else Future.successful("{success: false, message: session already exist}")
+          service match {
+            case "nails" =>
+              (nailsActor ? requestJson).mapTo[Boolean].flatMap { ans =>
+                if(ans) {
+                  Future.successful("{success: true, message: new nails session}")
+                } else Future.successful("{success: false, message: session is already exist}")
+              }
+
+            case "hairCut" =>
+              (hairActor ? requestJson).mapTo[Boolean].flatMap { ans =>
+                if (ans) {
+                  Future.successful("{success: true, message: new hairCut session}")
+                } else Future.successful("{success: false, message: session is already exist}")
+              }
+
+            case "brows" =>
+              (browsActor ? requestJson).mapTo[Boolean].flatMap { ans =>
+                if (ans) {
+                  Future.successful("{success: true, message: new brows session}")
+                } else Future.successful("{success: false, message: session is already exist}")
+              }
+
+            case _ =>
+              logger.info("unknown service")
+              Future.successful("{success: false, message: unknown service}")
           }
         }
         complete(resultFuture)
